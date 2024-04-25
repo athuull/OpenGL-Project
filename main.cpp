4905495724a19974b88416e877086fc1726875ec
athuull <athulvanil03@gmail.com>
2024-04-25 13:59:12 +0100

    Rendered a double sided triangle / quad

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const char* vertexShaderSource =  "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+	"}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);\n"
+"}\n\0";
 void processInput(GLFWwindow* window);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
 int main() {
 	// Initialises GLFW
 	glfwInit();
@@ -24,9 +38,76 @@ int main() {
 
 	// Makes the window the current context
 	glfwMakeContextCurrent(window);
-
 	// Loads Glad to manage OpenGL function pointers
 	gladLoadGL();
+
+	// Vertex Shader
+	 int success;
+	 char infoLog[512];
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); // Creates vertex Shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);  // Sets the Shader source
+	glCompileShader(vertexShader);  // Compiles the Shader
+
+   // Vertex Shader Error Checking/Handling
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);   
+	glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+	if (!success) {
+		std::cout << infoLog << std::endl;
+	}
+     // Fragment Shader
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+	if (!success)
+		std::cout << infoLog << std::endl;
+	// Shader Program
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Shader Program Error checking / handling
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+	if (!success)
+		std::cout << infoLog << std::endl;
+
+	float firstTriangle[] = {
+		0.0f, 0.5f,0.0f,
+		-0.5f , 0.0f,0.0f,
+		0.5f , 0.0f,0.0f
+
+	};
+	float secondTriangle[] = { 0.0f, -0.5f,0.0f,
+		-0.5f , 0.0f,0.0f,
+		0.5f , 0.0f,0.0f };
+		
+
+
+
+	unsigned int VBOs[2], VAOs[2];
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs);
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	
+	
+
+	
+
+	
 	while (!glfwWindowShouldClose(window)) {
 
 		// Process Key Inputs
@@ -35,11 +116,20 @@ int main() {
 		// Render
 		glClearColor(1.0f, 0.5f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAOs[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// then we draw the second triangle using the data from the second VAO
+		glBindVertexArray(VAOs[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+
 
 		//Swaps Front and Back Buffers and handles events in the window
 		glfwSwapBuffers(window);
 		glfwPollEvents(); 
-	
+		glfwSetKeyCallback(window, key_callback);
+
 	}
 
 	glfwTerminate();
@@ -47,7 +137,19 @@ int main() {
 }
 
 void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
+	}
+	
+}
+// Code for toggling between wireframe mode and fill mode.
+bool wireframeMode = false;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_W && action == GLFW_PRESS)
+	{
+		wireframeMode = !wireframeMode;
+		glPolygonMode(GL_FRONT_AND_BACK, wireframeMode ? GL_LINE : GL_FILL);
 	}
 }
